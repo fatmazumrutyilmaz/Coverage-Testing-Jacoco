c4fee7186afc176ce747b68ef4e7d1a933d10b81
Fatma Zümrüt Yılmaz <fatmazumrutyilmaz@gmail.com>
2019-03-05 20:41:20 +0300

    TestUtil Coverage 100%

--- src/test/java/TestUtil.java
+++ src/test/java/TestUtil.java
@@ -4,13 +4,44 @@ import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 public class TestUtil {
   Util c;
 
+  @Rule
+  public ExpectedException exception=ExpectedException.none();
+
   @Before
-  public void setUp() { c = new Util(); }
+  public void setUp() { 
+    c = new Util(); 
+  }
 
   @Test
-  public void example() { assertTrue(true); }
+  public void example() { 
+    assertTrue(true); 
+  }
+  @Test
+  public void testLength1(){
+    assertFalse(c.compute(1));
+  }
+  @Test
+  public void testLength2(){
+    assertFalse(c.compute(1,2));
+  }
+  @Test
+  public void testThrowsRuntimeException(){
+    exception.expect(RuntimeException.class);
+    c.compute(1,2,4,5,0);
+  }
+  @Test
+  public void testSumMod(){
+    assertTrue(c.compute(2,3,4));
+  }
+  @Test
+  public void testSumNotMod(){
+    assertFalse(c.compute(2,5,10));
+  }
+
 }
