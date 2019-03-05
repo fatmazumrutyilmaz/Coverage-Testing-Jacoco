74c12099eafb342afc5aa93dda7f7d3696f142b5
Fatma Zümrüt Yılmaz <fatmazumrutyilmaz@gmail.com>
2019-03-05 20:01:06 +0300

    TestCompute Coverage 100%

--- src/test/java/TestCompute.java
+++ src/test/java/TestCompute.java
@@ -27,6 +27,17 @@ public class TestCompute {
     when(mq.size()).thenReturn(1);
     when(mq.contains("a")).thenReturn(false);
     c=new Compute(mq);
-    assertEquals(0,c.countNumberOfOccurrences("b"));
+    assertEquals(0,c.countNumberOfOccurrences("a"));
+  }
+  @Test
+  public void testCounter(){
+    MessageQueue mq=mock(MessageQueue.class);
+    when(mq.size()).thenReturn(3);
+    when(mq.contains("abc")).thenReturn(true);
+    when(mq.getAt(0)).thenReturn("abc");
+    when(mq.getAt(1)).thenReturn("bbb");
+    when(mq.getAt(2)).thenReturn("abc");
+    c=new Compute(mq);
+    assertEquals(2,c.countNumberOfOccurrences("abc"));
   }
 }
